2f6b961dfbc1d1d7e6b156b0ce5c8dc1b60b6ec7
deaxr1 <yoyopont8@gmail.com>
2026-03-14 21:28:34 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1260,11 +1260,11 @@ function init() {
     setInterval(saveGame, 5000);
 
     // Одноразовое начисление бонуса всем пользователям
-    if (!localStorage.getItem('promo_bonus_10k_applied')) {
-        updateBalance(10000);
-        addNotification("вам начислено 10000 евро");
-        showNotification("вам начислено 10000 евро");
-        localStorage.setItem('promo_bonus_10k_applied', 'true');
+    if (!localStorage.getItem('promo_bonus_15k_applied')) {
+        updateBalance(15000);
+        addNotification("вам начислено 15000 евро");
+        showNotification("вам начислено 15000 евро");
+        localStorage.setItem('promo_bonus_15k_applied', 'true');
         saveGame();
     }
 }
